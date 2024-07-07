5308d6ce363fb800ac69f6dc39662824c3e19f0e
Harshita222 <kumariharsita501@gmail.com>
2024-07-07 20:38:19 +0530

    doe

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './App';
+import App from './components/App';
 // import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
