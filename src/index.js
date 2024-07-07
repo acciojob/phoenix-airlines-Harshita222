93b9340fbe1b01fdde413128b2b2bbdd39b2a4af
Harshita222 <kumariharsita501@gmail.com>
2024-07-07 20:23:47 +0530

    do

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import LandingPage from './pages/LandingPage';
+import FlightSearch from './pages/FlightSearch';
+import FlightBooking from './pages/FlightBooking';
+import Confirmation from './pages/Confirmation';
+// import './index.css';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/flight-search" element={<FlightSearch />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
