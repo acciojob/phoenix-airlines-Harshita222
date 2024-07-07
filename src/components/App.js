f1ddfacb678ea7ba38eec441cc0caa345ad4c5e3
Harshita222 <kumariharsita501@gmail.com>
2024-07-07 20:36:12 +0530

    do

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage';
+import FlightSearch from '../pages/FlightSearch';
+import FlightBooking from '../pages/FlightBooking';
+import Confirmation from '../pages/Confirmation';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/flight-search">Search Flights</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/flight-search" component={FlightSearch} />
+          <Route path="/flight-booking" component={FlightBooking} />
+          <Route path="/confirmation" component={Confirmation} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import LandingPage from './pages/LandingPage';
-import FlightSearch from './pages/FlightSearch';
-import FlightBooking from './pages/FlightBooking';
-import Confirmation from './pages/Confirmation';
+import App from './App';
 // import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/flight-search" element={<FlightSearch />} />
-        <Route path="/flight-booking" element={<FlightBooking />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
+      <App />
     </Router>
   </Provider>
 );
