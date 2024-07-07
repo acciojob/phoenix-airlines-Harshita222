8dba465ed1df8783171c63390604f0e86fb24c3d
Harshita222 <kumariharsita501@gmail.com>
2024-07-07 20:42:31 +0530

    doe

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import FlightSearch from '../pages/FlightSearch';
 import FlightBooking from '../pages/FlightBooking';
@@ -8,22 +7,20 @@ import Confirmation from '../pages/Confirmation';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/flight-search">Search Flights</Link></li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/" exact component={LandingPage} />
-          <Route path="/flight-search" component={FlightSearch} />
-          <Route path="/flight-booking" component={FlightBooking} />
-          <Route path="/confirmation" component={Confirmation} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/flight-search">Search Flights</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/flight-search" element={<FlightSearch />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './components/App';
+import App from './App';
 // import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
